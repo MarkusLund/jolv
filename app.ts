03d90d80dc01416218ec878709e1372625024647
Markus Lund <markuslund92@gmail.com>
2023-04-04 00:28:10 +0200

    inital commit

---
+++ app.ts
@@ -0,0 +1,115 @@
+const dice1 = document.getElementById("dice1")!;
+const dice2 = document.getElementById("dice2")!;
+const rollButton = document.getElementById("roll")!;
+const sumButton = document.getElementById("sum")!;
+const chooseButtons = document.getElementById("chooseButtons");
+const diceButton = document.getElementById("dice")!;
+const numbersContainer = document.getElementById("numbers-container")!;
+let dice1roll = 1;
+let dice2roll = 1;
+const numbersToAcchieve = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+let achievedNumbers: number[] = [];
+
+function checkIfSetAndListIsIdentical(a: Set<number>, b: number[]) {
+  return a.size === b.length && [...a].every((value) => b.includes(value));
+}
+
+function checkWin() {
+  if (checkIfSetAndListIsIdentical(numbersToAcchieve, achievedNumbers)) {
+    alert("You won!");
+    // achievedNumbers.clear();
+    dice1.innerText = "";
+    dice2.innerText = "";
+
+    const numberElements = numbersContainer.children;
+
+    for (let i = numberElements.length - 1; i >= 0; i--) {
+      const numberElement = numberElements[i];
+      numberElement.classList.add("missing");
+    }
+  }
+}
+
+function checkLoss() {
+  if (
+    new Set(achievedNumbers).size !== achievedNumbers.length ||
+    Math.max(...achievedNumbers) > Math.max(...numbersToAcchieve)
+  ) {
+    alert("Game Over!");
+    resetGame();
+  }
+}
+
+function resetGame() {
+  dice1.innerText = "⚅";
+  dice2.innerText = "⚅";
+
+  const numberElements = numbersContainer.children;
+
+  for (let i = numberElements.length - 1; i >= 0; i--) {
+    const numberElement = numberElements[i];
+    numberElement.classList.add("missing");
+  }
+}
+
+function updateNumbersContainer() {
+  const numberElements = numbersContainer.children;
+
+  for (let i = 0; i < numberElements.length; i++) {
+    const numberElement = numberElements[i];
+
+    if (achievedNumbers.includes(Number(numberElement.innerText))) {
+      numberElement.classList.remove("missing");
+    } else {
+      numberElement.classList.add("missing");
+    }
+  }
+}
+
+function addEachToAcchievedNumbers(numbers: number[]) {
+  numbers.forEach((number: number) => {
+    achievedNumbers.push(number);
+  });
+  updateNumbersContainer();
+  checkLoss();
+  checkWin();
+  chooseButtons.classList.add("hidden");
+  rollButton.classList.remove("hidden");
+}
+
+sumButton.addEventListener("click", () => {
+  addEachToAcchievedNumbers([Number(dice1roll) + Number(dice2roll)]);
+});
+
+diceButton.addEventListener("click", () => {
+  addEachToAcchievedNumbers([Number(dice1roll), Number(dice2roll)]);
+});
+
+rollButton.addEventListener("click", () => {
+  dice1roll = Math.floor(Math.random() * 6) + 1;
+  dice2roll = Math.floor(Math.random() * 6) + 1;
+  dice1.innerText = numberToDiceImage(dice1roll);
+  dice2.innerText = numberToDiceImage(dice2roll);
+  // Display choosebuttons
+  chooseButtons.classList.remove("hidden");
+  rollButton.classList.add("hidden");
+});
+
+function numberToDiceImage(number: number) {
+  switch (number) {
+    case 1:
+      return "⚀";
+    case 2:
+      return "⚁";
+    case 3:
+      return "⚂";
+    case 4:
+      return "⚃";
+    case 5:
+      return "⚄";
+    case 6:
+      return "⚅";
+    default:
+      return number.toString();
+  }
+}
